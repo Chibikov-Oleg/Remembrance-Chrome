24a528998fc690586ed017038f1baa7efd15842e
Chibikov Oleg <oleg_chiobikov@epam.com>
2017-01-14 20:18:10 +0300

    Initial

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,52 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(details => {
+	console.log('previousVersion', details.previousVersion);
+});
+
+var sendTextToRemembrance = function (text) {
+	$.ajax({
+		url: 'http://localhost:2033/api/words/',
+		type: 'PUT',
+		dataType: 'json',
+		contentType: 'application/json',
+		processData: false,
+		data: JSON.stringify(text),
+		error: function (e) {
+			if (!e.responseText)
+				console.log('Remembrance is not launched.');
+			else
+				console.log(e.responseText);
+		},
+	});
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function (details) {
+	var parameter = details.url.split('/').pop();
+	if (parameter) {
+		console.log('Text to send to Remembrance: ' + parameter);
+		sendTextToRemembrance(parameter);
+	}
+	chrome.tabs.remove(details.tabId);
+	return {
+		close: true,
+		redirectUrl: 'javascript:'
+	};
+}, {
+	urls: ['http://localhost:2033/api/words/*'],
+	types: ['main_frame']
+},
+	['blocking']);
+
+chrome.contextMenus.removeAll();
+
+chrome.contextMenus.create({
+	'title': 'Send to Remembrance',
+	'contexts': ['selection'],
+	'onclick': function (e) {
+		if (!e.selectionText)
+			return;
+		sendTextToRemembrance(e.selectionText);
+	}
+});

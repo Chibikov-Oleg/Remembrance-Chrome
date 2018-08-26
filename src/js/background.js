cceca04278cdc8ac40041f567386eee4582c1f2b
Scar <ochibikov@gmail.com>
2018-08-26 19:50:12 +1000

    Webpack

---
+++ src/js/background.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const url = "http://localhost:2053/api/words/";
+
+chrome.runtime.onInstalled.addListener(details => {
+  console.log("previousVersion", details.previousVersion);
+});
+
+function request(method, url, json) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    const req = new XMLHttpRequest();
+    req.open(method, url);
+    req.setRequestHeader("Content-Type", "application/json");
+
+    req.onload = function() {
+      // This is called even on 404 etc so check the status
+      if (req.status >= 200 && req.status < 300) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      } else {
+        // Otherwise reject with the response or status text which will hopefully be a meaningful
+        // error
+        reject(Error(req.responseText || req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error());
+    };
+
+    // Make the request
+    req.send(json);
+  });
+}
+
+const sendTextToRemembrance = text => {
+  request("PUT", url, JSON.stringify(text)).catch(function(e) {
+    if (!e.message) alert("Remembrance is not launched.");
+    else alert(e);
+  });
+};
+
+const hideWindowAsync = () => {
+  return new Promise(function(resolve, reject) {
+    chrome.windows.getCurrent(function(win) {
+      chrome.windows.update(win.id, { state: "minimized" });
+      resolve();
+    });
+  });
+};
+
+function removeTab(tabId) {
+  return new Promise(function(resolve, reject) {
+    chrome.tabs.remove(tabId, resolve);
+  });
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  async function(details) {
+    const parameter = details.url.split("/").pop();
+    if (parameter) {
+      console.log("Text to send to Remembrance: " + parameter);
+      sendTextToRemembrance(parameter);
+    }
+    await removeTab(details.tabId);
+    await hideWindowAsync();
+    return {
+      cancel: true
+    };
+  },
+  {
+    urls: [url + "*"],
+    types: ["main_frame"]
+  },
+  ["blocking"]
+);
+
+chrome.contextMenus.removeAll();
+
+chrome.contextMenus.create({
+  title: "Send to Remembrance",
+  contexts: ["selection"],
+  onclick: function(e) {
+    if (!e.selectionText) return;
+    sendTextToRemembrance(e.selectionText);
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const outputRelativePath = "build";
+const outputPath = path.join(__dirname, outputRelativePath);
+
+module.exports = {
+  mode: "production",
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "src/manifest.json", to: "" },
+      { from: "src/images", to: "images" },
+      { from: "src/locales", to: "_locales" }
+    ]),
+    new CleanWebpackPlugin(outputRelativePath),
+    new webpack.ProgressPlugin()
+  ],
+  entry: {
+    background: "./src/js/background.js",
+    chromereload: "./src/js/chromereload.js"
+  },
+  output: {
+    path: outputPath,
+    filename: "scripts/[name].js",
+    sourceMapFilename: "debugging/[file].map"
+  }
+};
